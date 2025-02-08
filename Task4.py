dc54b385ad11550b900e7f87602e4c662683b2fb
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-08 13:30:58 +0500

    Write a program to convert Celsius to Fahrenheit

---
+++ Task4.py
@@ -0,0 +1,7 @@
+# 4.	Write a program to convert Celsius to Fahrenheit
+
+print("Enter the Celsius value : ", end = "")
+celsius = float(input())
+fahrenheit = (celsius*9/5)+32
+
+print(f" {celsius} Celsius =  {fahrenheit} Fahrenheit ")
