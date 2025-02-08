a3a034ddef49a153dc8362aa75bbea385b649107
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-08 13:09:19 +0500

    Write a program to calculate the area of a circle, where the radius is input by the user.

---
+++ Task2.py
@@ -0,0 +1,6 @@
+# 2.	Write a program to calculate the area of a circle, where the radius is input by the user.
+
+print("Enter the radius to find the area of circle:", end ="")
+radius = int(input())
+area = 2*3.14*radius
+print(f"Area of circle with radius {radius} is : {area}")
