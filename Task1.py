8d6e7ac28a9bf021c67027756a5c71e6e3f487d2
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-08 12:51:50 +0500

    Write a program to input your name and age, and print them

---
+++ Task1.py
@@ -0,0 +1,8 @@
+# 1.	Write a program to input your name and age, and print them
+
+print("Enter you name:", end = " ")
+name = input()
+print("Enter your age:", end = " ")
+age = int(input())
+
+print(f"Your name is {name} and your age is {age}")
