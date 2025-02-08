c5efe45f127596188997d4740d711d3933ee228d
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-08 22:37:08 +0500

    Revert "Write a program to display the multiplication table of a number entered by the user."
    This reverts commit 0e0202a43d95276df5b02833970c7d82a0c3304d.

--- Task11.py
+++
@@ -1,5 +0,0 @@
-# 11.	Write a program to display the multiplication table of a number entered by the user.
-
-number = int(input("Enter a number for which you want to print a table : "))
-for i in range(1,11):
-    print(f"{i} * {number} = {i*number}")
