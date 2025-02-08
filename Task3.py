a47e839c5c5cdbc48f46b5a5726b40db6d26156d
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-08 13:18:48 +0500

    Write a program to swap two numbers using a temporary variable.

---
+++ Task3.py
@@ -0,0 +1,12 @@
+#3.	Write a program to swap two numbers using a temporary variable.
+
+a = 4
+b = 5
+
+print(f"Printing numbers Before Swapping a = {a} , b = {b}")
+
+temp = a
+a = b
+b = temp
+
+print(f"Printing numbers After Swapping a = {a} , b = {b}")
