171e55bf7c3e4040948cf980937970ac33c4626f
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-12 23:53:45 +0500

    deleting task10 bc of wrong name

--- 10_Task.py
+++
@@ -1,8 +0,0 @@
-# 10.	Write a program to calculate the sum of numbers from 1 to n, where n is entered by the user
-n = int(input("Enter a number to calculate the sum from 1 to n: "))
-sum = 0
-
-for i in range(1, n + 1):
-    sum += i  
-
-print(f"The sum of numbers from 1 to {n} is {sum}")
