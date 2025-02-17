44d659e9f7fd2531b1211ed6c772f67a49e0d39b
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-02-17 15:41:49 +0500

    Write a program to replace all spaces in a string with an underscore (_).

---
+++ 25_Task.py
@@ -0,0 +1,4 @@
+# 25.	Write a program to replace all spaces in a string with an underscore (_).
+text = input("Enter a string: ")
+modified_text = text.replace(" ", "_")
+print("Modified string:", modified_text)
