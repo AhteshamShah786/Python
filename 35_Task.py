5cd2ae3cb7f0dc13c6a4bae1910fe5941e01ec1b
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-03-07 22:21:51 +0500

    Write a Python program to count the number of occurrences of each character in a given string

---
+++ 35_Task.py
@@ -0,0 +1,10 @@
+#Write a Python program to count the number of occurrences of each character in a given string.
+
+def char_count(string):
+    counts = {}
+    for char in string:
+        counts[char] = counts.get(char, 0) + 1
+    return counts
+
+string = "hello world"
+print(char_count(string))
