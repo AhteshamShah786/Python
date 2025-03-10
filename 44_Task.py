1f659f6e7278576a8bc2153dd168f3cf55ca8208
AhteshamShah786 <ahteshamshah11@gmail.com>
2025-03-10 20:41:05 +0500

    Write a program to generate a list of the first n prime numbers.

---
+++ 44_Task.py
@@ -0,0 +1,20 @@
+# 44.	Write a program to generate a list of the first n prime numbers.
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def generate_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+print(generate_primes(10))  # Example usage
